3f2fbaaa3046bdbc1cad19b53f834c41f6119146
sohini062003 <145773628+sohini062003@users.noreply.github.com>
2025-05-18 11:47:03 +0530

    Update summarizer.py

--- summarizer.py
+++ summarizer.py
@@ -13,6 +13,7 @@ import os
 load_dotenv()
 GROQ_API_KEY = os.getenv("GROQ_API_KEY")
 nltk.download('punkt')
+nltk.download('punkt_tab')
 
 def preprocess_text(text):
     text = re.sub(r'\s+', ' ', text)
